00f703f7efe4275d98370151a56152fb233206b5
chhatraptirohidas <chhatrarohidas@gmail.com>
2022-03-28 11:34:59 +0530

    initial

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const static_path = path.join(__dirname, "../client");
+
+app.use(express.static(static_path));
+
+app.use('/', (req,res) =>{
+    res.sendFile(path.join(__dirname + '/client/index.html'));
+})
+
+ app.listen(port, ()=>{
+     console.log(`Server Satrted in ${port}`);
+ })
