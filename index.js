37b704cc33f3d00df4c466f602370824e4ff7205
chhatraptirohidas <chhatrarohidas@gmail.com>
2022-03-28 11:51:10 +0530

    rerooted package.json

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const static_path = path.join(__dirname, "../client");
+const static_path = path.join(__dirname, "/client");
 
 app.use(express.static(static_path));
 
 app.use('/', (req,res) =>{
-    res.sendFile(path.join(__dirname + '/client/index.html'));
+    res.sendFile(path.join(__dirname + 'client/index.html'));
 })
 
  app.listen(port, ()=>{
